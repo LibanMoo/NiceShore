6cbd598fe09220c7a59be8d674323dc0f0c2df78
healpluspc1 <libanmohamedreal@gmail.com>
2026-05-19 16:43:38 +0300

    feat: created the users table that stores users info

---
+++ server/NiceshoreServer/models/user.go
@@ -0,0 +1,18 @@
+package models
+
+import (
+	"github.com/google/uuid"
+)
+
+type UserModel struct {
+	BaseModel
+
+	ID         uuid.UUID `gorm:"type:uuid;primaryKey;"`
+	Username   string    `gorm:"unique;not null"`
+	FullName   string    `gorm:"not null"`
+	Email      string    `gorm:"unique;not null"`
+	Password   string    `gorm:"not null"`
+	AvatarURL  string
+	Reputation string
+	IsActive   bool `gorm:"default:false"`
+}
