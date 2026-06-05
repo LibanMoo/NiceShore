6141978db473af2d0095cff60a2dd35b1ae9410c
LibanMoo <libanmohamedreal@gmail.com>
2026-06-05 15:32:48 +0300

    feat: created the auth file for authorization related routes

---
+++ server/NiceshoreServer/router/http/auth_routes.go
@@ -0,0 +1,13 @@
+package http
+
+import (
+	"github.com/LibanMoo/NiceShore/server/NiceshoreServer/logic/auth"
+	"github.com/gin-gonic/gin"
+)
+
+func AuthRoutes(router *gin.Engine) {
+	authGroup := router.Group("/api/auth")
+	{
+		authGroup.POST("signup", auth.Signup)
+	}
+}
