5414d001765d12705d44a3279571a4bdc00114e5
LibanMoo <libanmohamedreal@gmail.com>
2026-06-05 15:32:03 +0300

    feat: made the signup file

---
+++ server/NiceshoreServer/logic/auth/signup.go
@@ -0,0 +1,64 @@
+package auth
+
+import (
+	"net/http"
+
+	"github.com/LibanMoo/NiceShore/server/NiceshoreServer/models"
+	"github.com/LibanMoo/NiceShore/server/NiceshoreServer/repository"
+	"github.com/LibanMoo/NiceShore/server/NiceshoreServer/utils"
+	"github.com/gin-gonic/gin"
+)
+
+type SignupRequest struct {
+	Username string
+	Email    string
+	Password string
+}
+
+func Signup(c *gin.Context) {
+
+	var request SignupRequest
+
+	if err := c.ShouldBindJSON(&request); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	// Check if the email is already registered
+	existingUser, _ := repository.GetUserByEmail(request.Email)
+
+	if existingUser != nil {
+		c.JSON(http.StatusConflict, gin.H{
+			"error": "Email is already registered",
+		})
+		return
+	}
+
+	hashedPassword, err := utils.HashPassword(request.Password)
+	if err != nil {
+		return
+	}
+
+	user := models.User{
+		Username: request.Username,
+		Email:    request.Email,
+		Password: hashedPassword,
+	}
+
+	err = repository.CreateUser(&user)
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "Failed to create User",
+		})
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{
+		"message": "user created sucessfully",
+		"user_id": user.ID,
+	})
+
+}
