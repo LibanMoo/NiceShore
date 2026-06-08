4ed7f6c8e225a87ae3a4357181296f3cf16b98e0
LibanMoo <libanmohamedreal@gmail.com>
2026-06-08 15:06:43 +0300

    feat: made a repository file that inserts refresh_tokens to the database

---
+++ server/NiceshoreServer/repository/refresh_token_repository.go
@@ -0,0 +1,18 @@
+package repository
+
+import (
+	"github.com/LibanMoo/NiceShore/server/NiceshoreServer/database/postgres"
+	"github.com/LibanMoo/NiceShore/server/NiceshoreServer/models"
+)
+
+func CreateRefreshToken(token *models.RefreshToken) error {
+	return postgres.DB.Create(token).Error
+}
+
+func GetRefreshToken(token string) (*models.RefreshToken, error) {
+	var refreshToken models.RefreshToken
+	if err := postgres.DB.Where("token = ?", token).First(&refreshToken).Error; err != nil {
+		return nil, err
+	}
+	return &refreshToken, nil
+}
