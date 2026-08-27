4affeae72585084f359f1f34f51329adf84ad91d
LibanMoo <libanmohamedreal@gmail.com>
2026-08-27 15:09:05 +0300

    chore: updated the users model

--- server/NiceshoreServer/models/user.go
+++ server/NiceshoreServer/models/user.go
@@ -1,18 +1,19 @@
 package models
 
 import (
+	"time"
+
 	"github.com/google/uuid"
 )
 
 type User struct {
 	BaseModel
 
-	ID         uuid.UUID `gorm:"type:uuid;default:gen_random_uuid();primaryKey;"`
-	Username   string    `gorm:"unique;not null"`
-	FullName   string    `gorm:"not null"`
-	Email      string    `gorm:"unique;not null"`
-	Password   string    `gorm:"not null"`
-	AvatarURL  string
-	Reputation string
-	IsActive   bool `gorm:"default:false"`
+	ID        uuid.UUID `gorm:"type:uuid;default:gen_random_uuid();primaryKey;"`
+	FullName  string    `gorm:"not null"`
+	Email     string    `gorm:"unique;not null"`
+	Password  string    `gorm:"not null"`
+	AvatarURL string
+	IsActive  bool      `gorm:"default:false"`
+	Dob       time.Time `gorm:"type:date"`
 }
