8c64c874bdadd3ca99a8ce8fe03305ce1782f04c
LibanMoo <libanmohamedreal@gmail.com>
2026-09-13 21:06:52 +0300

    chore: changed the id column to uuid and added dob column

--- server/NiceshoreServer/database/migrations/20260827103226_create_users_table.sql
+++ server/NiceshoreServer/database/migrations/20260827103226_create_users_table.sql
@@ -1,11 +1,13 @@
 -- +goose Up
 SELECT 'up SQL query';
 
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
 create table users (
-    id serial primary key,
-    name varchar(255) not null,
+    id uuid primary key default gen_random_uuid(),
+    full_name varchar(255) not null,
     email varchar(255) unique not null,
     password varchar(255) not null,
+    dob date,
     created_at timestamp default current_timestamp,
     updated_at timestamp default current_timestamp
 );  
